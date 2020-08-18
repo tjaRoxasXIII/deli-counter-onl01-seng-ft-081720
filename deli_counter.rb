0a9404dada2f7f5bd64e025a9cbc87fd12e4a1ee
Taylor Allen <tjaroguetech@gmail.com>
2020-08-18 21:20:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+def line(katz_deli)
+  position = 1
+  new_array = []
+  if katz_deli == [] 
+    puts "The line is currently empty."
+  else
+    katz_deli.each do |name|
+      new_array.push("#{position}. #{name}")
+      position += 1
+    end
+    puts "The line is currently: #{new_array}"
+  end
+end 
+
+def take_a_number(katz_deli, name)
+  
+end 
