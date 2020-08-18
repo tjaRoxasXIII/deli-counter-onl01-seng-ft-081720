34e39c1263ffb653bb9719aa3891e82513e69a0d
Taylor Allen <tjaroguetech@gmail.com>
2020-08-18 21:32:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -26,7 +26,7 @@ end
 
 def now_serving(katz_deli)
   if katz_deli == []
-    puts "There is nobdy waiting to be served!"
+    puts "There is nobody waiting to be served!"
   elsif katz_deli.length > 0
     puts "Now Serving:"
   end
